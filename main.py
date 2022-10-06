91fce69ade4ab878e5c5877d0ac6e4deae7aa341
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-10-06 19:51:20 +0200

    Cuenta commits, detecta el lenguaje

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from ast import If
 from github import Github
 
 # Introducimos la token del usuario
-g = Github("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")
+print("Key:")
+key = input()
+g = Github(key)
+repo_path = "ivanmiguelmolinero/Scappe-Room"
 
-
-
-repo = g.get_repo("github/choosealicense.com")
-contents = repo.get_contents("")
-for content_file in contents:
+# Introducimos la dirección del repo
+repo = g.get_repo(repo_path)
+contents = repo.get_contents("") # Obtenemos su contenido
+for content_file in contents: # Comprobamos si alguno es el "LICENSE"
     if (content_file.name[:7] == "LICENSE"):
         print("TIENE LICENCIA. CHECK")
-        file = content_file.name;
+        file = content_file.name
         print("Content name:", content_file.name)
+
+# Contamos los commits
+for repoGit in g.get_user().get_repos():
+    print(repoGit.name, repoGit.get_commits().totalCount)
+
+# Mostramos los commits del repo seleccionado
+print(repo.name)
+print("COMMITS: ", repo.get_commits().totalCount)
+
+# Mostramos lenguajes utilizados
+print("Lenguaje: ", list(repo.get_languages().keys()))
