57d367b3446d618f0534e87b0dc8e15a10b8e84e
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-11-02 17:43:00 +0100

    Algunas notas y primera plantilla

---
+++ Django/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Para el URL raíz muestro la lista de posts
+    path('', views.post_list, name='post_list'),
+    # Para el URL de la imagen muestro la imagen
+    path('github.png', views.post_img, name='post_img')
+]
--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,3 +1,12 @@
+from email.mime import image
 from django.shortcuts import render
 
 # Create your views here.
+
+# Función que recibe la petición post_list y devuelve un HTML
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+# Función que recibe la petición post_img y devuelve la imagen
+def post_img(request):
+    return render(request, 'blog/github.png', content_type='image/png')
