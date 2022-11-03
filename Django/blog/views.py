6b84bda9fb0cd993b0ece1690ecb9a052c0db00d
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-11-03 16:10:47 +0100

    Formularios y seguridad añadida

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -20,3 +21,34 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     return render(request, 'blog/post_detail.html', {'post': post})
+
+# Función que recibe la petición post_new y devuelve la página para añadir un post
+def post_new(request):
+    if request.method == "POST": # Si hemos rellenado el formulario con un nuevo post y queremos enviarlo
+        form = PostForm(request.POST) # Rellenamos el formulario con los datos del POST
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else: # Si es la primera vez que entramos
+        form = PostForm()
+    
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+# Función que recible la petición post_edit y devuelve o bien la página para editar o bien el post ya editado
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # Básicamente el if de la función de arriba
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
