4746a2bf9e72d2cc93aee6d75616dd54e74033d0
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-12-27 22:04:12 +0100

    Pestaña comunidad analizada

--- Django_BRR/OpenBRR/views.py
+++ Django_BRR/OpenBRR/views.py
@@ -106,8 +106,107 @@ def post_repo(request):
 
 def get_data(request):
     if request.method == 'GET':
-        commits = request.GET['commits']
-        return render(request, 'OpenBRR/result.html', {'commits': commits})
+        commits = int(request.GET['commits'])
+        valor_commits = float(request.GET['valor-commits'])
+        nota_commits = calc_nota_commits(commits) 
+        nota_commits_pond = nota_commits * (valor_commits/100) #-- Nota ponderada con su peso
+        forks = int(request.GET['forks'])
+        valor_forks = float(request.GET['valor-forks'])
+        nota_forks = calc_nota_forks(forks) 
+        nota_forks_pond = nota_forks * (valor_forks/100) #-- Nota ponderada con su peso
+        suscriptores = int(request.GET['suscriptores'])
+        valor_suscriptores = float(request.GET['valor-suscriptores'])
+        nota_sus = calc_nota_sus(suscriptores)
+        nota_sus_pond = nota_sus * (valor_suscriptores/100) #-- Nota ponderada con su peso
+        organization = request.GET['organization']
+        valor_organizacion = float(request.GET['valor-organización'])
+        nota_org = calc_nota_org(organization)
+        nota_org_pond = nota_org * (valor_organizacion/100) #-- Nota ponderada con su peso
+        update = request.GET['update']
+        valor_update = float(request.GET['valor-actualización'])
+        nota_upd = calc_nota_update(update)
+        nota_upd_pond = nota_upd * (valor_update/100) #-- Nota ponderada con su peso
+        nota_comunidad = nota_commits_pond + nota_forks_pond + nota_sus_pond + nota_org_pond + nota_upd_pond
+        return render(request, 'OpenBRR/result.html', {'nota_commits': nota_commits,
+                                                        'nota_forks': nota_forks,
+                                                        'nota_suscriptores': nota_sus,
+                                                        'nota_org': nota_org,
+                                                        'nota_update': nota_upd,
+                                                        'nota_comunidad': nota_comunidad})
+
+def calc_nota_commits(com):
+    # Calculamos la nota de los commits
+    if (com > 20) or (com <= 20):
+        nota_commits = 2.5
+    elif (com > 20) or (com <= 60):
+        nota_commits = 5
+    elif (com > 60) or (com <= 100):
+        nota_commits = 7.5
+    elif com > 100:
+        nota_commits = 10
+    else:
+        nota_commits = 0
+    return nota_commits #-- Nota ponderada con su peso
+
+def calc_nota_forks(forks):
+    # Calculamos la nota de los forks
+    if (forks > 0) or (forks <= 50):
+        nota_forks = 2.5
+    elif (forks > 500) or (forks <= 150):
+        nota_forks = 5
+    elif (forks > 160) or (forks <= 500):
+        nota_forks = 7.5
+    elif forks > 500:
+        nota_forks = 10
+    else:
+        nota_forks = 0
+    return nota_forks
+
+def calc_nota_sus(sus):
+    # Calculamos la nota de las suscripciones
+    if (sus > 20) or (sus <= 20):
+        nota_sus = 2.5
+    elif (sus > 20) or (sus <= 60):
+        nota_sus = 5
+    elif (sus > 60) or (sus <= 100):
+        nota_sus = 7.5
+    elif sus > 100:
+        nota_sus = 10
+    else:
+        nota_sus = 0
+    return nota_sus
+
+def calc_nota_org(org):
+    # Calculamos la nota de pertenecer o no a una organización
+    if org == 'Sí':
+        nota_org = 10
+    elif org == 'No':
+        nota_org = 0
+    return nota_org
+
+def calc_nota_update(upd):
+    # Calculamos la nota de la actualización
+    now = datetime.now()
+    day = to_valid_format(now.day)
+    month = to_valid_format(now.month)
+    year = str(now.year)
+    # Separamos el día, el mes y el año de la última actualización
+    upd_separado = upd.split('-')
+    if year == upd_separado[0]:
+        if month == upd_separado[1]:
+            if day == upd_separado[2]:
+                nota_upd = 10 # Se ha actualizado hoy
+            else:
+                nota_upd = 7.5 # Se ha actualizado este mes
+        else:
+            nota_upd = 5 # Se ha actualizado este año
+    else:
+        nota_upd = 2.5 # No se ha actualizado este año
+    return nota_upd
+
+def calc_nota_comunidad(com, v_com, forks, v_forks, sus, v_sus, org, v_org, upd, v_upd):
+    pass
+
 
 #-- Función que devuelve la fecha en formato correcto
 def to_valid_format(date):
